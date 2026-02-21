16885b41d4840635a1469403eeb17c670d199413
Arthur Bréant <arthur@thinkr.fr>
2026-02-21 16:29:44 +0100

    feat: add html_args

--- R/hx_page.R
+++ R/hx_page.R
@@ -16,6 +16,8 @@ hx_head <- function(..., title = "htmxr page") {
 #'
 #' @param ... page content. Use [hx_head()] to add elements to the head.
 #' @param lang language code for the `<html>` element (default `"en"`).
+#' @param html_attrs a named list of additional attributes to set on the
+#'   `<html>` element (e.g. `list("data-theme" = "cupcake")` for DaisyUI).
 #'
 #' @importFrom htmltools tags doRenderTags
 #'
@@ -23,21 +25,25 @@ hx_head <- function(..., title = "htmxr page") {
 #'   (including `<!DOCTYPE html>`), ready to be served as an HTTP response.
 #'
 #' @export
-hx_page <- function(..., lang = "en") {
+hx_page <- function(..., lang = "en", html_attrs = list()) {
   dots <- list(...)
   is_head <- vapply(dots, inherits, logical(1), "hx_head")
   head_extra <- unlist(dots[is_head], recursive = FALSE)
   body_content <- dots[!is_head]
 
-  page <- tags$html(
-    lang = lang,
-    tags$head(
-      tags$meta(charset = "UTF-8"),
-      tags$script(src = "/htmxr/assets/htmx/2.0.8/htmx.min.js"),
-      head_extra
-    ),
-    tags$body(body_content)
+  html_args <- c(
+    list(lang = lang),
+    html_attrs,
+    list(
+      tags$head(
+        tags$meta(charset = "UTF-8"),
+        tags$script(src = "/htmxr/assets/htmx/2.0.8/htmx.min.js"),
+        head_extra
+      ),
+      tags$body(body_content)
+    )
   )
 
+  page <- do.call(tags$html, html_args)
   paste0("<!DOCTYPE html>\n", doRenderTags(page) |> as.character())
 }
